b1ce3e349f3bd6408d139dda0ac94df278fdbd9e
Julien Gabryelewicz <julien.gabryelewicz2@gmail.com>
2023-08-20 11:49:03 +0200

    Add translate and crypt function

--- src/caesar.f90
+++ src/caesar.f90
@@ -2,6 +2,23 @@ module caesar
   implicit none
   public :: caesar_crypt
 contains
+
+  function get_index(index) result(new_index)
+    integer, optional :: index
+    integer :: new_index
+
+    if (present(index)) then
+      if (index<26 .and. index>0) then
+        new_index = index 
+      else
+        print *, 'Erreur : index doit être entre 1 et 25 inclus'
+      end if
+    else
+      new_index = 3
+    end if    
+
+  end function get_index
+
   function caesar_crypt(sentence,index) result(translated)
     character(:), allocatable :: sentence, translated
     integer, optional :: index
@@ -11,15 +28,7 @@ contains
     sentence = trim(sentence)
     translated = ""
 
-    if (present(index)) then
-      if (index<26 .and. index>0) then
-        index2 = index 
-      else
-        print *, 'Erreur : index doit être entre 1 et 25 inclus'
-      end if
-    else
-      index2 = 3
-    end if
+    index2 = get_index(index)
 
     do i=0,len(sentence)
       do j=65,90
@@ -35,4 +44,30 @@ contains
     end do
 
   end function caesar_crypt
+
+  function caesar_translate(sentence,index) result(translated)
+    character(:), allocatable :: sentence, translated
+    integer, optional :: index
+    integer :: index2
+    character :: ch
+    integer :: i,j
+    sentence = trim(sentence)
+    translated = ""
+
+    index2 = get_index(index)
+    
+    do i=0,len(sentence)
+      do j=65,90
+          ch=achar(j)
+          if (sentence(i+1:i+1) == ch) then
+            if (j<=65+index2) then
+              translated = translated // achar(j+26-index2)
+            else
+              translated = translated // achar(j-index2)
+            end if
+          end if
+      end do
+    end do
+
+  end function caesar_translate
 end module caesar
--- test/caesar_tests.f90
+++ test/caesar_tests.f90
@@ -1,14 +1,14 @@
 module caesar_tests
-  use caesar, only: caesar_crypt
+  use caesar, only: caesar_crypt, caesar_translate
 
   implicit none
 
   private
-  public :: test_caesar
+  public :: test_caesar_crypt, test_caesar_translate
 
 contains
 
-  subroutine test_caesar
+  subroutine test_caesar_crypt
 
     character(:), allocatable :: result,expect, sentence
     expect = "ERQMRXU"
@@ -30,12 +30,37 @@ contains
       print *, 'test failed with index'
     end if
 
-  end subroutine test_caesar
+  end subroutine test_caesar_crypt
+
+  subroutine test_caesar_translate
+
+    character(:), allocatable :: result,expect, sentence
+    sentence = "ERQMRXU"
+    expect = "BONJOUR"
+    result = caesar_translate(sentence)
+
+    if (result == expect) then
+      print *, 'test translate passed with no index'
+    else
+      print *, 'test translate failed with no index'
+    end if
+
+    sentence = "DQPLQWT"
+    result = caesar_translate(sentence,2)
+
+    if (result == expect) then
+      print *, 'test translate passed with index'
+    else
+      print *, 'test translate failed with index'
+    end if
+
+  end subroutine test_caesar_translate
 
 end module caesar_tests
 
 
 program run_tests
-  use caesar_tests, only: test_caesar
-  call test_caesar()
+  use caesar_tests, only: test_caesar_crypt, test_caesar_translate
+  call test_caesar_crypt()
+  call test_caesar_translate()
 end program run_tests
